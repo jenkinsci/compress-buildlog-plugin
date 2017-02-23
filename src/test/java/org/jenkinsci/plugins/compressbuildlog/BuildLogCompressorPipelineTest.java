2a19e2124081d19f227f27f09a76116c85d52700
Artur Harasimiuk <artur.harasimiuk@intel.com>
2017-02-23 14:27:23 +0100

    add test for isApplicable on WorkflowJob
    Create Pipeline project and pass its .class to make sure that
    isApplicable returns true.

---
+++ src/test/java/org/jenkinsci/plugins/compressbuildlog/BuildLogCompressorPipelineTest.java
@@ -0,0 +1,25 @@
+import org.jenkinsci.plugins.compressbuildlog.BuildLogCompressor;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+
+import org.jvnet.hudson.test.JenkinsRule;
+
+public class BuildLogCompressorPipelineTest {
+    @Rule
+    public JenkinsRule r = new JenkinsRule();
+
+    @Test
+    public void testIsApplicable() throws Exception {
+        WorkflowJob workflowJob = r.jenkins.createProject(WorkflowJob.class, "pipeline");
+        Assert.assertNotNull(workflowJob);
+
+        BuildLogCompressor.DescriptorImpl descriptor = new BuildLogCompressor.DescriptorImpl();
+        Assert.assertNotNull(descriptor);
+
+        boolean res = descriptor.isApplicable(workflowJob.getClass());
+        Assert.assertTrue(res);
+    }
+}
